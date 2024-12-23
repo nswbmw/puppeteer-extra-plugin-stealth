f7ae20a0bb834ece5b4b999fb2f15d529f471b95
nswbmw <4279697+nswbmw@users.noreply.github.com>
2024-12-23 15:16:54 +0800

    init

---
+++ evasions/navigator.languages/index.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const { PuppeteerExtraPlugin } = require('puppeteer-extra-plugin')
+const withUtils = require('../_utils/withUtils')
+
+/**
+ * Pass the Languages Test. Allows setting custom language & languages.
+ *
+ * @param {Object} [opts] - Options
+ * @param {Array<string>} [opts.languages] - The languages to use (default: `['en-US', 'en']`)
+ * @param {string} [opts.language] - The language to use (default: `'en-US'`)
+ */
+class Plugin extends PuppeteerExtraPlugin {
+  constructor(opts = {}) {
+    super(opts)
+  }
+
+  get name() {
+    return 'stealth/evasions/navigator.languages'
+  }
+
+  get defaults() {
+    return {
+      languages: [] // Empty default, otherwise this would be merged with user defined array override
+    }
+  }
+
+  async onPageCreated(page) {
+    const languages = this.opts.languages.length
+      ? this.opts.languages
+      : ['zh-CN', 'zh']
+    const language = this.opts.language || languages[0]
+
+    await withUtils(page).evaluateOnNewDocument(
+      (utils, { languages }) => {
+        utils.replaceGetterWithProxy(
+          Object.getPrototypeOf(navigator),
+          'languages',
+          utils.makeHandler().getterValue(Object.freeze([...languages]))
+        )
+      },
+      {
+        languages
+      }
+    )
+
+    await withUtils(page).evaluateOnNewDocument(
+      (utils, { language }) => {
+        utils.replaceGetterWithProxy(
+          Object.getPrototypeOf(navigator),
+          'language',
+          utils.makeHandler().getterValue(language)
+        )
+      },
+      {
+        language
+      }
+    )
+  }
+}
+
+module.exports = function (pluginConfig) {
+  return new Plugin(pluginConfig)
+}
